96d0d92d2499a5750f84da7a4fb0db2f721a28de
srivijaykalki <poosarla.s@northeastern.edu>
2024-01-24 18:24:43 -0500

    test

---
+++ content.js
@@ -0,0 +1,9 @@
+(function() {
+    alert('hello world');
+    // var connection = document
+    //  querySelectorALL(
+    //   '.invitation-card_action-btn.artdeco-button--secondary');
+    //  for(var i = 0; i < connection. length; i = أ + 1)
+    //    connection[i].click();
+    // document.body.appendChild(connection);
+     })();
